e13cf29e81d95b29bc4def17ea8a32b16a5816ae
( •_•) Ｒａｊｅｓｈ Ｃｈｏｗｄａｒｙ( •_•) <154892930+jettyrajeshchowdary@users.noreply.github.com>
2024-11-26 18:14:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,42 +24,42 @@ icon.addEventListener("click",()=>{
      loop:true
  })
 
-const form = document.getElementById('form');
-const result = document.getElementById('result');
+// const form = document.getElementById('form');
+// const result = document.getElementById('result');
 
-form.addEventListener('submit', function(e) {
-  e.preventDefault();
-  const formData = new FormData(form);
-  const object = Object.fromEntries(formData);
-  const json = JSON.stringify(object);
-  result.innerHTML = "Please wait..."
+// form.addEventListener('submit', function(e) {
+//   e.preventDefault();
+//   const formData = new FormData(form);
+//   const object = Object.fromEntries(formData);
+//   const json = JSON.stringify(object);
+//   result.innerHTML = "Please wait..."
 
-    fetch('https://api.web3forms.com/submit', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            body: json
-        })
-        .then(async (response) => {
-            let json = await response.json();
-            if (response.status == 200) {
-                result.innerHTML = "Form submitted successfully";
-            } else {
-                console.log(response);
-                result.innerHTML = json.message;
-            }
-        })
-        .catch(error => {
-            console.log(error);
-            result.innerHTML = "Something went wrong!";
-        })
-        .then(function() {
-            form.reset();
-            setTimeout(() => {
-                result.style.display = "none";
-            }, 3000);
-        });
-});
+//     fetch('https://api.web3forms.com/submit', {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//                 'Accept': 'application/json'
+//             },
+//             body: json
+//         })
+//         .then(async (response) => {
+//             let json = await response.json();
+//             if (response.status == 200) {
+//                 result.innerHTML = "Form submitted successfully";
+//             } else {
+//                 console.log(response);
+//                 result.innerHTML = json.message;
+//             }
+//         })
+//         .catch(error => {
+//             console.log(error);
+//             result.innerHTML = "Something went wrong!";
+//         })
+//         .then(function() {
+//             form.reset();
+//             setTimeout(() => {
+//                 result.style.display = "none";
+//             }, 3000);
+//         });
+// });//
 
