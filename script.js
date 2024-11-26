525940950b6cb0ac14cdc4d2677b5f1e12f1c80f
( •_•) Ｒａｊｅｓｈ Ｃｈｏｗｄａｒｙ( •_•) <154892930+jettyrajeshchowdary@users.noreply.github.com>
2024-11-26 18:02:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,3 +23,43 @@ icon.addEventListener("click",()=>{
      backDelay:1000,
      loop:true
  })
+<script>
+const form = document.getElementById('form');
+const result = document.getElementById('result');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const object = Object.fromEntries(formData);
+  const json = JSON.stringify(object);
+  result.innerHTML = "Please wait..."
+
+    fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: json
+        })
+        .then(async (response) => {
+            let json = await response.json();
+            if (response.status == 200) {
+                result.innerHTML = "Form submitted successfully";
+            } else {
+                console.log(response);
+                result.innerHTML = json.message;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            result.innerHTML = "Something went wrong!";
+        })
+        .then(function() {
+            form.reset();
+            setTimeout(() => {
+                result.style.display = "none";
+            }, 3000);
+        });
+});
+</script>
